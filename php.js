e39d6b122108d189423622f4782f61bb775fdd38
KYLE <106235228+kyle-inc@users.noreply.github.com>
2024-06-12 21:20:29 +0530

    Upload

---
+++ php.js
@@ -0,0 +1,27 @@
+function checkPswd() {
+    var confirmPassword = "adminconsole@kyle_gainaccess";
+    var password = document.getElementById("pswd").value;
+    if (password == confirmPassword) {
+        window.location = "admin.php";
+    }
+    else {
+        window.location = "action_page.html";
+        alert("Admin ID or Password is incorrect or mightbe empty");
+    }
+}
+
+
+// < !--Function to check password-- >
+//     <script type="text/javascript">
+//         function checkPswd() {
+//                 var confirmPassword = "adminconsole@kyle_gainaccess";
+//         var password = document.getElementById("pswd").value;
+//         if (password == confirmPassword) {
+//             window.location = "admin.php";
+//                 }
+//         else{
+//             window.location = "action_page.html";
+//         alert("Admin ID or Password is incorrect or mightbe empty");
+//                 }
+//             }
+//     </script>
