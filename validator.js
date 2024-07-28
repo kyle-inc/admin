d7e5925f226f308e90e71ebe34b300a81e980525
KYLE <kyle.web.in@gmail.com>
2024-07-28 12:17:23 +0530

    Update

--- validator.js
+++ validator.js
@@ -5,8 +5,7 @@ function checkPswd() {
         window.location = "admin.php";
     }
     else {
-        window.location = "https://kyle-inc.github.io/admin/action_page?email=";
-        alert("Admin ID or Password is incorrect or mightbe empty");
+        window.location = "https://kyle-inc.github.io/admin/action_page?password=";
     }
 }
 
