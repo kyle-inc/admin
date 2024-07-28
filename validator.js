56a239c73b723047a61705434a3cea3b2e812fc8
KYLE <kyle.web.in@gmail.com>
2024-07-28 12:25:40 +0530

    Update

--- validator.js
+++ validator.js
@@ -9,19 +9,3 @@ function checkPswd() {
         alert("Admin ID or Password is incorrect or mightbe empty");
     }
 }
-
-
-// < !--Function to check password-- >
-//     <script type="text/javascript">
-//         function checkPswd() {
-//                 var confirmPassword = "adminconsole@kyle_gainaccess";
-//         var password = document.getElementById("pswd").value;
-//         if (password == confirmPassword) {
-//             window.location = "admin.php";
-//                 }
-//         else{
-//             window.location = "action_page.html";
-//         alert("Admin ID or Password is incorrect or mightbe empty");
-//                 }
-//             }
-//     </script>
